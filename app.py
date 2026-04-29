3cda3ef2bc06d88e70495587423457ce5c008bc8
TALHA IFTIKHAR <156535212+talhaiftikhar001@users.noreply.github.com>
2026-04-29 22:41:23 +0500

    app.py  updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pyodbc
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session  # import flask
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
